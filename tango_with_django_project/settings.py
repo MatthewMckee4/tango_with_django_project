65c61a1e077b85514d1ce5e1dad1c99a44948de2
Matthew McKee <2772171m@student.gla.ac.uk>
2024-01-26 14:58:27 +0000

    Finished chapter 5

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -79,13 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "tango_with_django_project.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
